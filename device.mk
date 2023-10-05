0c7d81f7cc4a7872ebb0c5978d2d14db8c8b819c
Sushrut1101 <guptasushrut@gmail.com>
2023-10-05 12:31:21 +0530

    selene: Switch to AIDL Bootctrl service
    Change-Id: Icf80c1f76010d87a5fabbb395057b914bfaf8b18
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -120,9 +120,8 @@ PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot-service.default \
+    android.hardware.boot-service.default_recovery
 
 PRODUCT_PACKAGES += \
     libmtk_bsg \
