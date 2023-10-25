94f71d5ba5359231c22de6f7c8c6340bd38e6cc3
SamarV-121 <samarvispute121@pm.me>
2023-10-25 13:23:30 +0000

    selene: Install charger images to vendor
    Change-Id: Ic915f5991e672c250ebdc776093d33a22bab9aa7

--- device.mk
+++ device.mk
@@ -172,6 +172,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,system/ca-certificates/files/,$(TARGET_COPY_OUT_VENDOR)/etc/security/cacerts)
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images_vendor
+
 # Disable Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
